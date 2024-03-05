6eb59234ac6b20d3784fab34a9edb02330211d8a
Duo Wang <131799407+Daydreamer0305@users.noreply.github.com>
2024-03-05 15:24:38 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,224 @@
+# -*- coding:utf-8 -*-
+# @Time       :2023/12/18 20:36 AM
+# @AUTHOR     :Duo Wang
+# @FileName   :demo.py
+import torch
+import torch.nn as nn
+import torch.utils.data as Data
+from scipy.io import loadmat
+import numpy as np
+import time
+import os
+
+import argparse
+import logging
+from models.FDNet import FDNet
+from utils import (trPixel2Patch, tsPixel2Patch, set_seed,
+                   output_metric, train_epoch, valid_epoch, draw_classification_map)
+
+# -------------------------------------------------------------------------------
+# Parameter Setting
+parser = argparse.ArgumentParser(description="Training for FDNet")
+parser.add_argument('--gpu_id', default='0',
+                    help='gpu id')
+parser.add_argument('--seed', type=int, default=0,
+                    help='number of seed')
+parser.add_argument('--batch_size', type=int, default=64,
+                    help='number of batch size')
+parser.add_argument('--epochs', type=int, default=500,
+                    help='number of epoch')
+parser.add_argument('--dataset', choices=['Houston', 'Augsburg', 'Muufl', 'Dafeng'], default='Dafeng',
+                    help='dataset to use')
+parser.add_argument('--learning_rate', type=float, default=0.001,
+                    help='learning rate')
+parser.add_argument('--flag', choices=['train', 'test'], default='test',
+                    help='testing mark')
+parser.add_argument('--patch_size', type=int, default=8,
+                    help='cnn input size')
+parser.add_argument('--wavename', type=str, default='db2',
+                    help='type of wavelet')
+parser.add_argument('--attn_kernel_size', type=int, default=9,
+                    help='')
+parser.add_argument('--coefficient_hsi', type=float, default=0.7,
+                    help='weight of HSI data in feature fusion')
+parser.add_argument('--fae_embed_dim', type=int, default=64,
+                    help='number of channels in fae input data')
+parser.add_argument('--fae_depth', type=int, default=1,
+                    help='depth of fae')
+
+args = parser.parse_args()
+os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu_id)
+
+# -------------------------------------------------------------------------------
+# create log
+logger = logging.getLogger("Trainlog")
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+file_handler = logging.FileHandler("cls_logs/{}/{}_{}.log".format(args.dataset, args.flag, args.dataset))
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+# -------------------------------------------------------------------------------
+def train1time():
+    # -------------------------------------------------------------------------------
+    if args.dataset == 'Houston':
+        num_classes = 15
+        DataPath1 = 'Data/Houston/Houston.mat'
+        DataPath2 = 'Data/Houston/LiDAR.mat'
+        LabelPath = 'Data/Houston/train_test_gt.mat'
+        Data1 = loadmat(DataPath1)['HSI']
+        Data2 = loadmat(DataPath2)['LiDAR']
+    elif args.dataset == 'Augsburg':
+        num_classes = 7
+        DataPath1 = 'Data/Augsburg/data_HS_LR.mat'
+        DataPath2 = 'Data/Augsburg/data_DSM.mat'
+        Data1 = loadmat(DataPath1)['data_HS_LR']
+        Data2 = loadmat(DataPath2)['data_DSM']
+        LabelPath = 'Data/Augsburg/train_test_gt.mat'
+    elif args.dataset == 'Muufl':
+        num_classes = 11
+        DataPath1 = 'Data/Muufl/HSI.mat'
+        DataPath2 = 'Data/Muufl/LiDAR_DEM1.mat'
+        Data1 = loadmat(DataPath1)['HSI']
+        Data2 = loadmat(DataPath2)['LiDAR']
+        LabelPath = 'Data/Muufl/train_test_gt_60.mat'
+    elif args.dataset == 'Dafeng':
+        num_classes = 9
+        DataPath1 = 'Data/Dafeng/HSI_30ch.mat'
+        DataPath2 = 'Data/Dafeng/lidar_DSM_45.mat'
+        Data1 = loadmat(DataPath1)['HSI']
+        Data2 = loadmat(DataPath2)['lidar']
+        LabelPath = 'Data/Dafeng/train_test_gt_20.mat'
+    else:
+        raise "Requires correct dataset name!"
+
+    Data1 = Data1.astype(np.float32)  # hsi
+    Data2 = Data2.astype(np.float32)  # lidar
+    TrLabel = loadmat(LabelPath)['train_data']
+    TsLabel = loadmat(LabelPath)['test_data']
+
+    patchsize = args.patch_size  # input spatial size for CNN
+    pad_width = np.floor(patchsize / 2)
+    pad_width = int(pad_width)
+    TrainPatch1, TrainPatch2, TrainLabel = trPixel2Patch(
+        Data1, Data2, patchsize, pad_width, TrLabel)
+    TestPatch1, TestPatch2, TestLabel, _, _ = tsPixel2Patch(
+        Data1, Data2, patchsize, pad_width, TsLabel)
+
+    train_dataset = Data.TensorDataset(
+        TrainPatch1, TrainPatch2, TrainLabel)
+    test_dataset = Data.TensorDataset(
+        TestPatch1, TestPatch2, TestLabel)
+    train_loader = Data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True)
+    test_loader = Data.DataLoader(
+        test_dataset, batch_size=args.batch_size, shuffle=True)
+
+    [H1, W1, _] = np.shape(Data1)
+    Data2 = Data2.reshape([H1, W1, -1])
+    height1, width1, band1 = Data1.shape
+    height2, width2, band2 = Data2.shape
+    # data size
+    logger.info('\n')
+    logger.info("=" * 50)
+    logger.info("=" * 50)
+    logger.info("hsi_height={0},hsi_width={1},hsi_band={2}".format(height1, width1, band1))
+    logger.info("lidar_height={0},lidar_width={1},lidar_band={2}".format(height2, width2, band2))
+    # -------------------------------------------------------------------------------
+    # create model
+    model = FDNet(l1=band1, l2=band2, patch_size=args.patch_size, num_classes=num_classes,
+                  wavename=args.wavename, attn_kernel_size=args.attn_kernel_size, coefficient_hsi=args.coefficient_hsi,
+                  fae_embed_dim=args.fae_embed_dim, fae_depth=args.fae_depth)
+
+    model = model.cuda()
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs)
+    # -------------------------------------------------------------------------------
+    # train & test
+    if args.flag == 'train':
+        get_ts_result = False
+        logger.info("start training")
+        tic = time.time()
+        for epoch in range(args.epochs):
+            # train model
+            model.train()
+            train_acc, train_obj, tar_t, pre_t = train_epoch(
+                model, train_loader, criterion, optimizer)
+            OA1, AA1, Kappa1, CA1 = output_metric(tar_t, pre_t)
+            logger.info("Epoch: {:03d} | train_loss: {:.4f} | train_OA: {:.4f} | train_AA: {:.4f} | train_Kappa: {:.4f}"
+                        .format(epoch + 1, train_obj, OA1, AA1, Kappa1))
+            scheduler.step()
+
+        torch.save(model.state_dict(), 'cls_param/{}/FDNet_{}.pkl'.format(args.dataset, args.dataset))
+        toc = time.time()
+        model.eval()
+        model.load_state_dict(torch.load('cls_param/{}/FDNet_{}.pkl'.format(args.dataset, args.dataset)))
+        tar_v, pre_v = valid_epoch(model, test_loader, criterion, get_ts_result)
+        OA, AA, Kappa, CA = output_metric(tar_v, pre_v)
+        logger.info("Final records:")
+        logger.info("Maximal Accuracy: %f" % OA)
+        logger.info("OA: {:.4f} | AA: {:.4f} | Kappa: {:.4f}".format(OA, AA, Kappa))
+        logger.info(CA)
+        logger.info("Running Time: {:.2f}".format(toc - tic))
+
+    if args.flag == 'test':
+        # test best model
+        get_ts_result = False
+        tic_ts = time.time()
+        model.eval()
+        model.load_state_dict(torch.load('cls_param/{}/FDNet_{}.pkl'.format(args.dataset, args.dataset)))
+
+        tar_v, pre_v = valid_epoch(model, test_loader, criterion, get_ts_result)
+        OA, AA, Kappa, CA = output_metric(tar_v, pre_v)
+        toc_ts = time.time()
+        logger.info("Test records:")
+        logger.info("Maximal Accuracy: %f" % OA)
+        logger.info("OA: {:.4f} | AA: {:.4f} | Kappa: {:.4f}".format(OA, AA, Kappa))
+        logger.info(CA)
+        logger.info("Testing Time: {:.2f}".format(toc_ts - tic_ts))
+        logger.info("Parameter:")
+        logger.info(vars(args))
+
+        # draw map
+        if args.dataset == 'Houston':
+            TR_TS_Path = 'Data/Houston/tr_ts.mat'
+        elif args.dataset == 'Augsburg':
+            TR_TS_Path = 'Data/Augsburg/tr_ts.mat'
+        elif args.dataset == 'Muufl':
+            TR_TS_Path = 'Data/Muufl/tr_ts.mat'
+        elif args.dataset == 'Dafeng':
+            TR_TS_Path = 'Data/Dafeng/tr_ts.mat'
+        else:
+            raise "Correct dataset needed!"
+
+        TR_TS_Label = loadmat(TR_TS_Path)['tr_ts']
+        # draw gt map
+        draw_classification_map(TR_TS_Label, 'cls_map/{}/{}_groundTruth.png'.format(args.dataset, args.dataset),
+                                args.dataset)
+        # draw cls map
+        TR_TS_Patch1, TR_TS_Patch2, TR_TS_Label, xIndex_list, yIndex_list = tsPixel2Patch(
+            Data1, Data2, patchsize, pad_width, TR_TS_Label)
+        TR_TS_dataset = Data.TensorDataset(
+            TR_TS_Patch1, TR_TS_Patch2, TR_TS_Label)
+        best_test_loader = Data.DataLoader(
+            TR_TS_dataset, batch_size=args.batch_size, shuffle=False)
+
+        get_ts_result = True  # if True, return cls result
+        ts_result = valid_epoch(model, best_test_loader, criterion, get_ts_result)
+        ts_result_matrix = np.full((H1, W1), 0)
+        for i in range(len(ts_result)):
+            ts_result_matrix[xIndex_list[i], yIndex_list[i]] = ts_result[i]
+        draw_classification_map(ts_result_matrix, 'cls_map/{}/{}_predLabel.png'.format(args.dataset, args.dataset),
+                                args.dataset)
+
+
+if __name__ == '__main__':
+    set_seed(args.seed)
+    train1time()
---
+++ utils.py
@@ -0,0 +1,319 @@
+import torch
+import torch.backends.cudnn as cudnn
+from sklearn.metrics import confusion_matrix
+import numpy as np
+from PIL import Image
+
+
+def normalize(x):
+    norm_x = np.zeros(x.shape)
+    for i in range(x.shape[2]):
+        input2_max = np.max(x[:, :, i])
+        input2_min = np.min(x[:, :, i])
+        norm_x[:, :, i] = (x[:, :, i] - input2_min) / (input2_max - input2_min)
+
+    return norm_x
+
+
+class AverageMeter(object):
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.avg = 0
+        self.sum = 0
+        self.cnt = 0
+
+    def update(self, val, n=1):
+        self.sum += val * n
+        self.cnt += n
+        self.avg = self.sum / self.cnt
+
+
+def Accuracy(output, target, topk=(1,)):
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res, target, pred.squeeze()
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    cudnn.benchmark = False
+
+
+def output_metric(tar, pre):
+    matrix = confusion_matrix(tar, pre)
+    OA, AA_mean, Kappa, AA = cal_results(matrix)
+    return OA, AA_mean, Kappa, AA
+
+
+def cal_results(matrix):
+    shape = np.shape(matrix)
+    number = 0
+    sum = 0
+    AA = np.zeros([shape[0]], dtype=np.float64)
+    for i in range(shape[0]):
+        number += matrix[i, i]
+        AA[i] = matrix[i, i] / np.sum(matrix[i, :])
+        sum += np.sum(matrix[i, :]) * np.sum(matrix[:, i])
+    OA = number / np.sum(matrix)
+    AA_mean = np.mean(AA)
+    pe = sum / (np.sum(matrix) ** 2)
+    Kappa = (OA - pe) / (1 - pe)
+    return OA, AA_mean, Kappa, AA
+
+
+def trPixel2Patch(Data1, Data2, patchsize, pad_width, TR_Label):
+    """
+        Data1: hsi
+        Data2: lidar
+    """
+    [m1, n1, l1] = np.shape(Data1)
+    Data2 = Data2.reshape([m1, n1, -1])
+    [m2, n2, l2] = np.shape(Data2)
+
+    for i in range(l1):
+        Data1[:, :, i] = (Data1[:, :, i] - Data1[:, :, i].min()) / (Data1[:, :, i].max() - Data1[:, :, i].min())
+    x1 = Data1
+    for i in range(l2):
+        Data2[:, :, i] = (Data2[:, :, i] - Data2[:, :, i].min()) / (Data2[:, :, i].max() - Data2[:, :, i].min())
+    x2 = Data2
+
+    x1_pad = np.empty((m1 + patchsize, n1 + patchsize, l1), dtype='float32')
+    x2_pad = np.empty((m2 + patchsize, n2 + patchsize, l2), dtype='float32')
+    for i in range(l1):
+        temp = x1[:, :, i]
+        temp2 = np.pad(temp, pad_width, 'symmetric')
+        x1_pad[:, :, i] = temp2
+    for i in range(l2):
+        temp = x2[:, :, i]
+        temp2 = np.pad(temp, pad_width, 'symmetric')
+        x2_pad[:, :, i] = temp2
+
+    [ind1, ind2] = np.where(TR_Label > 0)
+    ind3 = ind1 + pad_width
+    ind4 = ind2 + pad_width
+    TrainNum = len(ind1)
+
+    TrainPatch1 = np.empty((TrainNum, l1, patchsize, patchsize), dtype='float32')
+    TrainPatch2 = np.empty((TrainNum, l2, patchsize, patchsize), dtype='float32')
+    TrainLabel = np.empty(TrainNum)
+
+    for i in range(len(ind1)):
+        patch1 = x1_pad[(ind3[i] - pad_width):(ind3[i] + pad_width), (ind4[i] - pad_width):(ind4[i] + pad_width), :]
+        patch1 = np.transpose(patch1, (2, 0, 1))
+        TrainPatch1[i, :, :, :] = patch1
+
+        patch2 = x2_pad[(ind3[i] - pad_width):(ind3[i] + pad_width), (ind4[i] - pad_width):(ind4[i] + pad_width), :]
+        patch2 = np.transpose(patch2, (2, 0, 1))
+        TrainPatch2[i, :, :, :] = patch2
+
+        patchlabel = TR_Label[ind1[i], ind2[i]]
+        TrainLabel[i] = patchlabel
+
+    TrainPatch1 = torch.from_numpy(TrainPatch1)
+    TrainPatch2 = torch.from_numpy(TrainPatch2)
+
+    TrainLabel = torch.from_numpy(TrainLabel) - 1
+    TrainLabel = TrainLabel.long()
+    return TrainPatch1, TrainPatch2, TrainLabel
+
+
+def tsPixel2Patch(Data1, Data2, patchsize, pad_width, TS_Label):
+    """
+        Data1: hsi
+        Data2: lidar
+    """
+    [m1, n1, l1] = np.shape(Data1)
+    Data2 = Data2.reshape([m1, n1, -1])
+    [m2, n2, l2] = np.shape(Data2)
+
+    for i in range(l1):
+        Data1[:, :, i] = (Data1[:, :, i] - Data1[:, :, i].min()) / (Data1[:, :, i].max() - Data1[:, :, i].min())
+    x1 = Data1
+    for i in range(l2):
+        Data2[:, :, i] = (Data2[:, :, i] - Data2[:, :, i].min()) / (Data2[:, :, i].max() - Data2[:, :, i].min())
+    x2 = Data2
+
+    x1_pad = np.empty((m1 + patchsize, n1 + patchsize, l1), dtype='float32')
+    x2_pad = np.empty((m2 + patchsize, n2 + patchsize, l2), dtype='float32')
+    for i in range(l1):
+        temp = x1[:, :, i]
+        temp2 = np.pad(temp, pad_width, 'symmetric')
+        x1_pad[:, :, i] = temp2
+    for i in range(l2):
+        temp = x2[:, :, i]
+        temp2 = np.pad(temp, pad_width, 'symmetric')
+        x2_pad[:, :, i] = temp2
+
+    [ind1, ind2] = np.where(TS_Label > 0)
+    ind3 = ind1 + pad_width
+    ind4 = ind2 + pad_width
+    TestNum = len(ind1)
+
+    TestPatch1 = np.empty((TestNum, l1, patchsize, patchsize), dtype='float32')
+    TestPatch2 = np.empty((TestNum, l2, patchsize, patchsize), dtype='float32')
+    TestLabel = np.empty(TestNum)
+
+    for i in range(len(ind1)):
+        patch1 = x1_pad[(ind3[i] - pad_width):(ind3[i] + pad_width), (ind4[i] - pad_width):(ind4[i] + pad_width), :]
+        patch1 = np.transpose(patch1, (2, 0, 1))
+        TestPatch1[i, :, :, :] = patch1
+
+        patch2 = x2_pad[(ind3[i] - pad_width):(ind3[i] + pad_width), (ind4[i] - pad_width):(ind4[i] + pad_width), :]
+        patch2 = np.transpose(patch2, (2, 0, 1))
+        TestPatch2[i, :, :, :] = patch2
+
+        patchlabel = TS_Label[ind1[i], ind2[i]]
+        TestLabel[i] = patchlabel
+
+    TestPatch1 = torch.from_numpy(TestPatch1)
+    TestPatch2 = torch.from_numpy(TestPatch2)
+
+    TestLabel = torch.from_numpy(TestLabel) - 1
+    TestLabel = TestLabel.long()
+    return TestPatch1, TestPatch2, TestLabel, ind1, ind2
+
+
+def train_epoch(model, train_loader, criterion, optimizer):
+    objs = AverageMeter()
+    top1 = AverageMeter()
+    tar = np.array([])
+    pre = np.array([])
+
+    for batch_idx, (batch_data1, batch_data2, batch_target) in enumerate(train_loader):
+        batch_data1 = batch_data1.cuda()
+        batch_data2 = batch_data2.cuda()
+        batch_target = batch_target.cuda()
+
+        optimizer.zero_grad()
+        batch_pred = model(batch_data1, batch_data2)
+        loss = criterion(batch_pred, batch_target)
+        loss.backward()
+        optimizer.step()
+
+        prec1, t, p = Accuracy(batch_pred, batch_target, topk=(1,))
+        n = batch_data1.shape[0]
+        objs.update(loss.data, n)
+        top1.update(prec1[0].data, n)
+        tar = np.append(tar, t.data.cpu().numpy())
+        pre = np.append(pre, p.data.cpu().numpy())
+
+    return top1.avg, objs.avg, tar, pre
+
+
+def valid_epoch(model, valid_loader, criterion, get_ts_result):
+    objs = AverageMeter()
+    top1 = AverageMeter()
+    tar = np.array([])
+    pre = np.array([])
+    # add
+    test_result = []
+
+    for batch_idx, (batch_data1, batch_data2, batch_target) in enumerate(valid_loader):
+        batch_data1 = batch_data1.cuda()
+        batch_data2 = batch_data2.cuda()
+        batch_target = batch_target.cuda()
+
+        batch_pred = model(batch_data1, batch_data2)
+        loss = criterion(batch_pred, batch_target)
+
+        prec1, t, p = Accuracy(batch_pred, batch_target, topk=(1,))
+        n = batch_data1.shape[0]
+        objs.update(loss.data, n)
+        top1.update(prec1[0].data, n)
+        tar = np.append(tar, t.data.cpu().numpy())
+        pre = np.append(pre, p.data.cpu().numpy())
+
+        # add
+        _, batch_pred_result = batch_pred.max(1)
+        test_result.extend((batch_pred_result + 1).cpu().numpy())
+
+    if get_ts_result:
+        return test_result
+
+    return tar, pre
+
+
+def draw_classification_map(label_matrix, img_path: str, dataset_name: str = 'Houston'):
+    if dataset_name == 'Houston':
+        color_map = {
+            0: (0, 0, 0),
+            1: (219, 94, 86),  # Healthy grass
+            2: (219, 147, 86),  # Stressed grass
+            3: (219, 200, 86),  # Synthetic grass
+            4: (184, 219, 86),  # Tree
+            5: (131, 219, 86),  # Soil
+            6: (86, 219, 94),  # Water
+            7: (86, 219, 147),  # Residential
+            8: (86, 219, 200),  # Commercial
+            9: (86, 184, 219),  # Road
+            10: (85, 130, 217),  # Highway
+            11: (93, 85, 216),  # Railway
+            12: (145, 85, 216),  # Park lot 1
+            13: (195, 84, 214),  # Park lot 2
+            14: (216, 85, 182),  # Tennis court
+            15: (216, 85, 129),  # Running track
+        }
+    elif dataset_name == 'Augsburg':
+        color_map = {
+            0: (0, 0, 0),
+            1: (56, 108, 52),  # Forest
+            2: (228, 55, 38),  # Residential Area
+            3: (229, 240, 84),  # Industrial Area
+            4: (170, 228, 49),  # Low Plants
+            5: (139, 222, 38),  # Allotment
+            6: (173, 238, 235),  # Commercial Area
+            7: (85, 132, 192),  # Water
+        }
+    elif dataset_name == 'Muufl':
+        color_map = {
+            -1: (0, 0, 0),
+            1: (59, 132, 70),  # Trees
+            2: (83, 172, 71),  # Mostly grass
+            3: (0, 204, 204),  # Mixed ground surface
+            4: (146, 82, 52),  # Dirt and sand
+            5: (218, 50, 43),  # Road
+            6: (103, 189, 199),  # Water
+            7: (229, 229, 240),  # Buildings shadow
+            8: (199, 177, 202),  # Buildings
+            9: (218, 142, 51),  # Sidewalk
+            10: (224, 220, 83),  # Yellow curb
+            11: (228, 119, 90),  # Cloth panels
+        }
+    elif dataset_name == 'Dafeng':
+        color_map = {
+            1: (59, 132, 70),  # Suaeda glauca
+            2: (0, 204, 204),  # Bare soil
+            3: (146, 82, 52),  # Road
+            4: (218, 50, 43),  # Buildings
+            5: (125, 21, 22),  # Water
+            6: (56, 86, 159),  # Reed
+            7: (251, 209, 26),  # S. alterniflora
+            8: (145, 85, 216),  # Algae
+            9: (81, 204, 237),  # Grass
+        }
+    else:
+        raise 'datasets name error'
+
+    label_matrix = np.array(label_matrix).astype(np.uint8)
+    rgb_array = np.zeros((label_matrix.shape[0], label_matrix.shape[1], 3), dtype=np.uint8)
+
+    for class_id, color in color_map.items():
+        rgb_array[label_matrix == class_id] = color
+
+    rgb_image = Image.fromarray(rgb_array)
+    rgb_image.save(img_path)
